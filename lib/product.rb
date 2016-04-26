8aec9867807e4777867316ca4537702288d182a9
Stefan Pavikevik <stefan_pavikevik@outlook.com>
2016-04-26 13:25:25 +0200

    Uses create_finder_methods to dynamically define finder methods.

--- lib/product.rb
+++ lib/product.rb
@@ -2,7 +2,7 @@ require_relative 'udacidata'
 
 class Product < Udacidata
   attr_reader :id, :price, :brand, :name
-
+  create_finder_methods :name, :brand
   def initialize(opts={})
 
     # Get last ID from the database if ID exists
